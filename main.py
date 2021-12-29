9708643033258f5566ff6449ffae2dec35d93eb8
stanzi1626 <stansfield_alex@yahoo.com>
2021-12-29 17:50:53 +0000

    Slime class stores array of previous postions with with an associated intensity

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import numpy as np
 from slime_class import slime
 
 time_step = 1
-number_ants = 1
+number_ants = 5
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 1000), ylim=(0, 1000))
+fig = plt.figure() 
+ax = plt.axes(xlim=(0, 1000), ylim=(0, 1000)) 
+line, = ax.plot([], [], 'ro',lw=2) 
 
-patches = []
-for i in range(number_ants):
-    patches.append(plt.Circle((0, 0), radius=5, fill = True, color = 'red'))
 colony = [slime(np.random.rand(2)*1000, np.random.uniform(-1, 1), np.random.uniform(-1, 1)) for _ in range(number_ants)]
 
 ################################################################
 
 def init():
-    for i in range(len(patches)):
-        patches[i].center = (0, 0)
-        ax.add_patch(patches[i])
-    
-    return patches
+    line.set_data([], [])
+    return line,
 
 def animate(i):
-    for j, k in enumerate(patches):
-        (a, b) = (colony[j].update(time_step))
-        print(colony[j].trail())
-        k.center = (a, b)
+    xdata = []
+    ydata = []
+    for j, k in enumerate(colony):
+        x, y = k.update(time_step)
+        k.trail() 
+        xdata.append(x)
+        ydata.append(y)  
+    
+    line.set_data(xdata, ydata)
 
-    return patches
+    return line,
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, frames=4000, interval=1, blit=True, repeat=False)
 
--- slime_class.py
+++ slime_class.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class slime():
     
     def __init__(self, initial_position, x_velocity, y_velocity):
@@ -5,9 +7,8 @@ class slime():
         self.y_position = initial_position[1]
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
-        
-        self.trail_xdata = []
-        self.trail_ydata = []
+
+        self.trail_array =np.zeros((0, 2)) #the first position contains the coordinates of the trail while the second position contains the intensity
     
     def update(self, time_step):
         self.x_position += self.x_velocity * time_step
@@ -20,10 +21,13 @@ class slime():
         return self.x_position, self.y_position
     
     def trail(self):
-        self.trail_xdata.append(self.x_position)
-        self.trail_ydata.append(self.y_position)
-        if len(self.trail_xdata) > 10:
-            self.trail_xdata.pop(0)
-            self.trail_ydata.pop(0)
+
+        self.trail_array = np.vstack((np.array(([self.x_position, self.y_position], 1.01)), self.trail_array))
+
+        for count, array in enumerate(self.trail_array):
+            array[1] -= 0.01
+            array[1] = float("{:.2f}".format(array[1]))
+            if array[1] <= 0:
+                self.trail_array = np.delete(self.trail_array, count, axis=0)
         
         return self.trail_xdata, self.trail_ydata
